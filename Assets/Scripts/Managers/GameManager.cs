32a18b8e0c85727161ff4e2d12472dc098464b56
CameronAIE <s221308@students.aie.edu.au>
2024-07-29 15:53:07 +0930

    made cursor

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,11 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class GameManager : MonoBehaviour
 {
     // Static reference
     public static GameManager Instance { get; private set; }
+
+    public bool addingControllers;
+
+    [Tooltip("The list of level scenes to be picked randomly (CAPITALIZATION MATTERS)")]
+    [SerializeField]
+    private string[] levels;
+
+    [Tooltip("the prefab players use to controll the character screen")]
+    [SerializeField]
+    private GameObject cursorPrefab;
 
     // Singleton instantiation
     private void Awake()
@@ -20,6 +33,10 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    //input variables
+    private List<Gamepad> m_controllers = new List<Gamepad>(); // list of connected controllers
+    private List<PlayerController> m_activePlayerControllers; // currently instantiated players
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +46,19 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(addingControllers)
+        {
+            for (int i = 0; i < Gamepad.all.Count; i++)
+            {
+                //check if the current gamepad has a button pressed and is not stored
+                if (Gamepad.all[i].allControls.Any(x => x is ButtonControl button && x.IsPressed() && !x.synthetic) && !m_controllers.Contains(Gamepad.all[i]))
+                {
+                    //store controller and add player to leaderboard
+                    m_controllers.Add(Gamepad.all[i]);
+
+                    GameObject newPlayer = PlayerInput.Instantiate(cursorPrefab, controlScheme: "Gamepad", pairWithDevice: Gamepad.all[i]).gameObject;
+                }
+            }
+        }
     }
 }
---
+++ Assets/Scripts/Player/CursorController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CursorController : MonoBehaviour
+{
+    [Tooltip("the speed this character will move at")]
+    [SerializeField]
+    private float speed;
+
+    private Vector3 movementDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += movementDirection * speed * Time.deltaTime;
+    }
+
+    /// <summary>
+    /// this function is called by the players input controller wich will change the vector the charcater moves by
+    /// </summary>
+    /// <param name="context"></param>
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementDirection.y = context.ReadValue<Vector2>().y;
+        movementDirection.x = context.ReadValue<Vector2>().x;
+    }
+}
