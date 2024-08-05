7ac438da8b077613f7529d502ade334f2ed8b256
CameronAIE <s221308@students.aie.edu.au>
2024-08-05 15:02:37 +0930

    added ball attack to penguin

---
+++ Assets/Scripts/Player/Attacks/SpawnObjectAtTransform.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectAtTransform : MonoBehaviour
+{
+    private CharacterBase player;
+
+    [Tooltip("the object to be created at the players SpawnProjectile object")]
+    [SerializeField]
+    private GameObject projectile;
+
+    [Tooltip("how long before this object destroys itself (wont destroy itself if set to 0)")]
+    [SerializeField]
+    private float lifetime;
+
+    [Tooltip("where the projectile will be spawned")]
+    [SerializeField]
+    private Transform _spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterBase>();
+    }
+
+    public void SpawnObjectAtSpawn()
+    {
+        GameObject newProjectile = Instantiate(projectile, _spawn.position, player.transform.rotation);
+
+        newProjectile.GetComponent<DamagePlayerOnCollision>().SetOwner(player);
+
+        if (lifetime != 0)
+        {
+            Destroy(newProjectile, lifetime);
+        }
+    }
+}
