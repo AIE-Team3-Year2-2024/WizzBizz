4c0149860b2f439c799f3a77df43fdfe564bc05e
CameronAIE <s221308@students.aie.edu.au>
2024-08-12 16:32:50 +0930

    should fix not destroying when created in a wall

--- Assets/Scripts/Projectiles/DestroyOnCollision.cs
+++ Assets/Scripts/Projectiles/DestroyOnCollision.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DestroyOnCollision : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
         Destroy(gameObject);
     }
