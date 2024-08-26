cddb5b10368a16851d512ba2f39a42ac71db7b1f
Ethan Harrison <128756346+EthanHarrison-AIE@users.noreply.github.com>
2024-08-26 13:35:30 +0930

    Testing scaling UI size
    Didn't work though smh

--- Assets/Scripts/UI/BillBoard.cs
+++ Assets/Scripts/UI/BillBoard.cs
@@ -5,25 +5,34 @@ using UnityEngine.UI;
 
 public class BillBoard : MonoBehaviour
 {
-    Camera camera;
+    Camera mainCamera;
+    [SerializeField] private Vector2 maxSize;
     [SerializeField] private Transform target;
     [SerializeField] private Vector3 offset;
 
     void Start()
     {
-        //This gets the main camera from the scene
+        // This gets the main camera from the scene
         if(Camera.main != null)
         {
-            camera = Camera.main;
-            //This enables main camera
-            camera.enabled = true;
+            mainCamera = Camera.main;
+            // This enables main camera
+            mainCamera.enabled = true;
         }
     }
 
-    // Setting the UI element's rotation to match the rotation of the camera and the UI's position to above the parent object.
+    
     void Update()
     {
-        transform.rotation = camera.transform.rotation;
+        // Adjusts UI rotation and position by world position.
+        transform.rotation = mainCamera.transform.rotation;
         transform.position = target.position + offset;
+
+        // Adjusts UI size by distance from main camera.
+        Vector3 uiPos = mainCamera.WorldToScreenPoint(transform.position);
+        Vector2 uiSize = new Vector2(maxSize.x * uiPos.z, maxSize.y * uiPos.y);
+        uiSize.x = Mathf.Clamp(uiSize.x, 0.0f, maxSize.x);
+        uiSize.y = Mathf.Clamp(uiSize.y, 0.0f, maxSize.y);
+        transform.localScale = uiSize;
     }
 }
