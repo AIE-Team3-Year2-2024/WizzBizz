e379e3002a11a54ef382925b7b45741d8796af07
Tom-J991 <126208954+Tom-J991@users.noreply.github.com>
2024-09-03 11:52:34 +0930

    Updated player movement, acceleration is no longer a parameter. Added knockback functionality as it's own component currently relying on the DamagePlayerOnCollision component to work, also has an AB test for knockback direction. Fixed a bug with the ice patch.

--- Assets/Scripts/Projectiles/AttackKnockback.cs
+++ Assets/Scripts/Projectiles/AttackKnockback.cs
@@ -7,11 +7,15 @@ public class AttackKnockback : MonoBehaviour
     [SerializeField]
     private float knockbackAmount = 3.0f;
 
+    [Header("AB Test, switches the knockback direction from either A: the direction of the contact (think of pool), or B: the direction of the projectile.")]
+    [SerializeField]
+    private bool abTest = false;
+
     public void DoKnockback(Collision coll, CharacterBase player)
     {
         if (coll.contactCount > 0)
         {
-            Vector3 knockBackDir = coll.GetContact(0).normal;
+            Vector3 knockBackDir = (abTest == false) ? coll.GetContact(0).normal : -transform.forward;
             player.TakeKnockback(knockbackAmount, knockBackDir);
         }
     }
